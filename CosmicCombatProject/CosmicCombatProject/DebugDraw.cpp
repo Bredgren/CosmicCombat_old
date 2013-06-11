841af791dd156151d4157549c4c5e0f6b9c6a1ba
Brandon Edgren <brandon.edgren@gmail.com>
2013-06-11 13:44:18 -0700

    Drawing now takes into account new pixels per meter setting.

--- CosmicCombatProject/CosmicCombatProject/DebugDraw.cpp
+++ CosmicCombatProject/CosmicCombatProject/DebugDraw.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "./DebugDraw.h"
+#include "./Settings.h"
 
 void DebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount,
                             const b2Color& color) {
@@ -11,11 +12,12 @@ void DebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount,
 
   sf::Color c(color.r*255, color.g*255, color.b*255);
   convex.setOutlineColor(c);
-  convex.setOutlineThickness(0.1f);
+  convex.setOutlineThickness(1.0f);
   convex.setFillColor(sf::Color::Transparent);
 
   for (int32 i = 0; i < vertexCount; ++i)
-    convex.setPoint(i, sf::Vector2f(vertices[i].x, vertices[i].y));
+    convex.setPoint(i, sf::Vector2f(vertices[i].x * kPixelsPerMeter,
+                                    vertices[i].y * kPixelsPerMeter));
 
   window_->draw(convex);
 }
@@ -29,7 +31,8 @@ void DebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount,
   convex.setFillColor(c);
 
   for (int32 i = 0; i < vertexCount; ++i)
-    convex.setPoint(i, sf::Vector2f(vertices[i].x, vertices[i].y));
+    convex.setPoint(i, sf::Vector2f(vertices[i].x * kPixelsPerMeter,
+                                    vertices[i].y * kPixelsPerMeter));
 
   window_->draw(convex);
 }
@@ -37,15 +40,16 @@ void DebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount,
 void DebugDraw::DrawCircle(const b2Vec2& center, float32 radius,
                            const b2Color& color) {
   sf::CircleShape circle;
-  circle.setRadius(radius);
+  circle.setRadius(radius * kPixelsPerMeter);
 
   sf::Color c(color.r*255, color.g*255, color.b*255);
   circle.setOutlineColor(c);
-  circle.setOutlineThickness(0.1f);
+  circle.setOutlineThickness(1.0f);
   circle.setFillColor(sf::Color::Transparent);
   circle.setPointCount(100);
 
-  circle.setPosition(center.x - radius, center.y - radius);
+  circle.setPosition((center.x - radius) * kPixelsPerMeter,
+                     (center.y - radius) * kPixelsPerMeter);
 
   window_->draw(circle);
 }
@@ -53,13 +57,14 @@ void DebugDraw::DrawCircle(const b2Vec2& center, float32 radius,
 void DebugDraw::DrawSolidCircle(const b2Vec2& center, float32 radius,
                                 const b2Vec2& axis, const b2Color& color) {
   sf::CircleShape circle;
-  circle.setRadius(radius);
+  circle.setRadius(radius * kPixelsPerMeter);
 
   sf::Color c(color.r*255, color.g*255, color.b*255);
   circle.setFillColor(c);
   circle.setPointCount(100);
 
-  circle.setPosition(center.x - radius, center.y - radius);
+  circle.setPosition((center.x - radius) * kPixelsPerMeter,
+                     (center.y - radius) * kPixelsPerMeter);
 
   window_->draw(circle);
 }
@@ -69,8 +74,8 @@ void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2,
   sf::Color c(color.r*255, color.g*255, color.b*255);
 
   sf::Vertex line[] = {
-    sf::Vertex(sf::Vector2f(p1.x, p1.y), c),
-    sf::Vertex(sf::Vector2f(p2.x, p2.y), c)
+    sf::Vertex(sf::Vector2f(p1.x * kPixelsPerMeter, p1.y * kPixelsPerMeter), c),
+    sf::Vertex(sf::Vector2f(p2.x * kPixelsPerMeter, p2.y * kPixelsPerMeter), c)
   };
 
   window_->draw(line, 2, sf::Lines);
@@ -78,7 +83,7 @@ void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2,
 
 void DebugDraw::DrawTransform(const b2Transform& xf) {
   b2Vec2 p1 = xf.p, p2, p3;
-  const float32 k_axisScale = 0.4f;
+  const float32 k_axisScale = 0.1f * kPixelsPerMeter;
 
   sf::Color c(255, 0, 0);
   sf::Color c2(0, 255, 0);
@@ -87,10 +92,10 @@ void DebugDraw::DrawTransform(const b2Transform& xf) {
   p3 = p1 + k_axisScale * xf.q.GetYAxis();
 
   sf::Vertex lines[] = {
-    sf::Vertex(sf::Vector2f(p1.x, p1.y), c),
-    sf::Vertex(sf::Vector2f(p2.x, p2.y), c),
-    sf::Vertex(sf::Vector2f(p1.x, p1.y), c2),
-    sf::Vertex(sf::Vector2f(p3.x, p3.y), c2)
+    sf::Vertex(sf::Vector2f(p1.x * kPixelsPerMeter, p1.y * kPixelsPerMeter), c),
+    sf::Vertex(sf::Vector2f(p2.x * kPixelsPerMeter, p2.y * kPixelsPerMeter), c),
+    sf::Vertex(sf::Vector2f(p1.x * kPixelsPerMeter, p1.y * kPixelsPerMeter), c2),
+    sf::Vertex(sf::Vector2f(p3.x * kPixelsPerMeter, p3.y * kPixelsPerMeter), c2)
   };
 
   window_->draw(lines, 4, sf::Lines);
--- CosmicCombatProject/CosmicCombatProject/main.cpp
+++ CosmicCombatProject/CosmicCombatProject/main.cpp
@@ -13,20 +13,26 @@ int main() {
     sf::Style::Titlebar | sf::Style::Close);
   window.setVerticalSyncEnabled(true);
   sf::View main_view(sf::Vector2f(0, 0), sf::Vector2f(800, 600));
-  main_view.zoom(0.1f);
+
+  //sf::Font font;
+  //if (!font.loadFromFile("C:/Windows/Fonts/arial.ttf"))
+  //  printf("error");
+  //sf::Text time_text;
+  //time_text.setFont(font);
+  //time_text.setCharacterSize(12);
+  //time_text.setColor(sf::Color::White);
 
   b2Vec2 gravity(0.0f, 10.0f);
   b2World world(gravity);
 
   DebugDraw debug_draw(&window);
-  if (kDrawDebugData) {
-    debug_draw.AppendFlags(b2Draw::e_shapeBit |
-                           b2Draw::e_jointBit |
-                           b2Draw::e_aabbBit |
-                           b2Draw::e_pairBit |
-                           b2Draw::e_centerOfMassBit);
-    world.SetDebugDraw(&debug_draw);
-  }
+  debug_draw.AppendFlags(b2Draw::e_shapeBit |
+                         b2Draw::e_jointBit |
+                         b2Draw::e_aabbBit |
+                         b2Draw::e_pairBit |
+                         b2Draw::e_centerOfMassBit);
+  world.SetDebugDraw(&debug_draw);
+
 
   b2BodyDef groundBodyDef;
   groundBodyDef.position.Set(0.0f, 10.0f);
@@ -51,13 +57,13 @@ int main() {
   bodyDef2.position.Set(0.0f, -7.0f);
   b2Body* body2 = world.CreateBody(&bodyDef2);
   b2PolygonShape dynamicBox2;
-  dynamicBox2.SetAsBox(1.0f, 1.0f);
+  dynamicBox2.SetAsBox(0.5f, 0.5f);
   b2FixtureDef fixtureDef2;
   fixtureDef2.shape = &dynamicBox2;
   fixtureDef2.density = 1.0f;
   fixtureDef2.friction = 0.3f;
   body2->CreateFixture(&fixtureDef2);
-  float32 timeStep = 1.0f / 60.0f;
+  float32 timeStep = 1.0f / 240.0f;
   int32 velocityIterations = 6;
   int32 positionIterations = 2;
 
@@ -68,13 +74,17 @@ int main() {
         window.close();
     }
 
-    world.Step(timeStep, velocityIterations, positionIterations);
+    for (int32 i = 0; i < 4; i++)
+      world.Step(timeStep, velocityIterations, positionIterations);
 
     window.setView(main_view);
     window.clear();
 
     if (kDrawDebugData)
       world.DrawDebugData();
+
+    //window.setView(window.getDefaultView());
+    //draw hud here
 
     window.display();
   }
